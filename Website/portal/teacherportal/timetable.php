5e3035a662605c52c592274c530061846d8505d0
hangSu123 <h6.su@connect.qut.edu.au>
2016-09-23 22:04:37 +1000

    Update timetable.php

--- Website/portal/teacherportal/timetable.php
+++ Website/portal/teacherportal/timetable.php
@@ -31,7 +31,7 @@ require "../../connect.inc";
 
   <header class="main-header">
     <!-- Logo -->
-    <a href="../../home.html" class="logo">
+    <a href="../../home.php" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>P</b>MS</span>
       <!-- logo for regular state and mobile devices -->
