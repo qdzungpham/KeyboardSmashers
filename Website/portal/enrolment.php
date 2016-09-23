a929d115a8b499ff19a5fa312202e23d70db4a59
hangSu123 <h6.su@connect.qut.edu.au>
2016-09-23 22:14:29 +1000

    Update enrolment.php

--- Website/portal/enrolment.php
+++ Website/portal/enrolment.php
@@ -46,7 +46,7 @@ require "../connect.inc";
       <!-- user menu -->
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
-        
+        <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
               <span class="hidden-xs"><?php echo $_SESSION["UserID"]; ?></span>
