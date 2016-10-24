1160832a83f7968286ef3db9e9f7c6488f5e982f
DarianCBrandt <darian.brandt@connect.qut.edu.au>
2016-10-25 02:23:19 +1000

    Added basic error checks and postDate field

--- Website/PHP/SQLAvailableJobs.php
+++ Website/PHP/SQLAvailableJobs.php
@@ -4,15 +4,25 @@
 	// Make the connect.inc file available for use
 	require '../connect.inc';
 	
+	// Initialise the error message
+	$errorMsg = "";
+	
 	// Regardless if this is an update or insertion, still need to make php variables of each field
 	$jobID=test_input($_POST["jobID"]);
 	$role=test_input($_POST["role"]);
 	$description=test_input($_POST["description"]);
+	$postDate=test_input($_POST["postDate"]);
+	
+	// Check for erraneous input values
+	if (empty($postDate)) {
+		$errorMsg = $errorMsg."<br>DATE POSTED field cannot be empty";
+	}
+	
 	
 	// Create the sql to create a new record
-	if (isset($_POST["createRecord"])){
-		$query="INSERT INTO availablejobs (role, description)
-			VALUES ('$role', '$description');";
+	if (isset($_POST["createRecord"]) && $errorMsg == ""){
+		$query="INSERT INTO availablejobs (role, description, postDate)
+			VALUES ('$role', '$description', '$postDate');";
 		$rs=$conn->prepare($query);
 		$rs->execute();
 		
@@ -20,9 +30,9 @@
 	}
 	
 	// Create the sql to update an old record
-	if (isset($_POST["updateRecord"])){
+	if (isset($_POST["updateRecord"]) && $errorMsg == ""){
 		$query="UPDATE availablejobs 
-			SET role='$role', description='$description'
+			SET role='$role', description='$description', postDate='$postDate'
 			WHERE jobID = ".$jobID.";";
 		$rs=$conn->prepare($query);
 		$rs->execute();
@@ -30,8 +40,10 @@
 		echo 'Successfully updated record';
 	}
 	
-	// Let the user go back to the main manager page
+	// Display the error messages and let the user go back to the main manager page
 	echo '
+		'. $errorMsg .'
+		<br><br>
 		<p><a href="../manager/main.php">Back to Manager Page</a></p>
 	';
 ?>
