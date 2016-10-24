7207e25052df61b83ad09d952eb4989de6fbdb6e
DarianCBrandt <darian.brandt@connect.qut.edu.au>
2016-10-25 02:27:58 +1000

    Added basic error checks

--- Website/PHP/SQLInstrumentHire.php
+++ Website/PHP/SQLInstrumentHire.php
@@ -4,6 +4,9 @@
 	// Make the connect.inc file available for use
 	require '../connect.inc';
 	
+	// Initialise the error message
+	$errorMsg = "";
+	
 	// Regardless if this is an update or insertion, still need to make php variables of each field
 	$hireID=test_input($_POST["hireID"]);
 	$studentID=test_input($_POST["studentID"]);
@@ -11,8 +14,28 @@
 	$startDate=test_input($_POST["startDate"]);
 	$endDate=test_input($_POST["endDate"]);
 	
+	// Check for erraneous input values
+	if (empty($studentID)) {
+		$errorMsg = $errorMsg."<br>STUDENT ID field cannot be empty";
+	}
+	if (!is_numeric($studentID)) {
+		$errorMsg = $errorMsg."<br>STUDENT ID field must be a number";
+	}
+	if (empty($instrumentID)) {
+		$errorMsg = $errorMsg."<br>INSTRUMENT ID field cannot be empty";
+	}
+	if (!is_numeric($instrumentID)) {
+		$errorMsg = $errorMsg."<br>INSTRUMENT ID field must be a number";
+	}
+	if (empty($startDate)) {
+		$errorMsg = $errorMsg."<br>HIRE DATE field cannot be empty";
+	}
+	if (empty($endDate)) {
+		$errorMsg = $errorMsg."<br>RETURN DATE field cannot be empty";
+	}
+	
 	// Create the sql to create a new record
-	if (isset($_POST["createRecord"])){
+	if (isset($_POST["createRecord"]) && $errorMsg == ""){
 		$query="INSERT INTO instrumenthire (studentID, instrumentID, startDate, endDate)
 			VALUES ('$studentID', '$instrumentID', '$startDate', 
 			'$endDate');";
@@ -23,7 +46,7 @@
 	}
 	
 	// Create the sql to update an old record
-	if (isset($_POST["updateRecord"])){
+	if (isset($_POST["updateRecord"]) && $errorMsg == ""){
 		$query="UPDATE instrumenthire 
 			SET studentID='$studentID', instrumentID='$instrumentID', startDate='$startDate', 
 			endDate='$endDate'
@@ -34,8 +57,10 @@
 		echo 'Successfully updated record';
 	}
 	
-	// Let the user go back to the main manager page
+	// Display the error messages and let the user go back to the main manager page
 	echo '
+		'. $errorMsg .'
+		<br><br>
 		<p><a href="../manager/main.php">Back to Manager Page</a></p>
 	';
 ?>
